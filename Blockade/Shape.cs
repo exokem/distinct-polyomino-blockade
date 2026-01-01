6fbd360840d53a32ce6d63a36949e25fdc1d0738
Zantron2000 <xanderpalmer7@gmail.com>
2026-01-01 12:36:24 -0500

    Converted the Shape class to use Vector2Int

--- Blockade/Shape.cs
+++ Blockade/Shape.cs
@@ -2,131 +2,81 @@ namespace Blockade;
 
 internal sealed partial class Shape
 {
-    static Dictionary<string, Shape> _shapes = new();
+    static readonly Dictionary<string, Shape> _shapes = [];
 
-    public static bool FromName(string name, out Shape shape)
-        => _shapes.TryGetValue(name, out shape);
+    public static bool FromName(string name, out Shape? shape)
+    {
+        return _shapes.TryGetValue(name, out shape);
+    }
 
     public static readonly Shape
         // Straight pieces
-        One = new("one", ["1"]),
-        Two = new("two", ["11"]),
-        Three = new("three", ["111"]),
-        Four = new("four", ["1111"]),
-        Five = new("five", ["11111"]),
+        One = new("one", [new Vector2Int(0, 0)]),
+        Two = new("two", [
+            new Vector2Int(0, 0),
+            new Vector2Int(1, 0)
+        ]),
+        Three = new("three", [
+            new Vector2Int(0, 0),
+            new Vector2Int(1, 0),
+            new Vector2Int(2, 0)
+        ]),
+        Four = new("four", [
+            new Vector2Int(0, 0),
+            new Vector2Int(1, 0),
+            new Vector2Int(2, 0),
+            new Vector2Int(3, 0)
+        ]),
+        Five = new("five", [
+            new Vector2Int(0, 0),
+            new Vector2Int(1, 0),
+            new Vector2Int(2, 0),
+            new Vector2Int(3, 0),
+            new Vector2Int(4, 0)
+        ]),
 
         Square = new("square", [
-            "11",
-            "11"
+            new Vector2Int(0, 0),
+            new Vector2Int(1, 0),
+            new Vector2Int(0, 1),
+            new Vector2Int(1, 1)
         ]),
         Lump = new("lump", [
-            "111",
-            "110"
-        ]),
-
-        Signpost = new("signpost", [
-            "011",
-            "110",
-            "010"
-        ]),
-
-        Steps = new("steps", [
-            "01",
-            "11"
-        ]),
-        Staircase = new("staircase", [
-            "001",
-            "011",
-            "110",
-        ]),
-
-        Cross = new("cross", [
-            "010",
-            "111",
-            "010",
-        ]),
-
-        Hook = new("hook", [
-            "111",
-            "001",
-        ]),
-        LongHook = new("long-hook", [
-            "1111",
-            "0001",
-        ]),
-
-        Lightning = new("lightning", [
-            "110",
-            "011",
-        ]),
-        LongLightning = new("long-lightning", [
-            "1100",
-            "0111",
-        ]),
-
-        Pike = new("pike", [
-            "1111",
-            "0100"
-        ]),
-
-        Corner = new("corner", [
-            "001",
-            "001",
-            "111",
-        ]),
-
-        Nail = new("nail", [
-            "111",
-            "010",
-        ]),
-        LongNail = new("long-nail", [
-            "111",
-            "010",
-            "010",
-        ]),
-
-        Bucket = new("bucket", [
-            "101",
-            "111"
-        ]),
-
-        Zed = new("zed", [
-            "110",
-            "010",
-            "011"
+            new Vector2Int(0, 0),
+            new Vector2Int(1, 0),
+            new Vector2Int(2, 0),
+            new Vector2Int(0, 1),
+            new Vector2Int(1, 1),
         ])
     ;
 }
 
 internal sealed partial class Shape
 {
-    readonly bool[][] _shape;
+    private readonly List<Vector2Int> _shape = new();
 
     public string Name { get; }
 
-    Shape(string name, bool[][] shape)
+    Shape(string name, List<Vector2Int> shape)
     {
         Name = name;
-        _shape = shape;
 
+        _shape = shape;
         _shapes[name] = this;
     }
 
-    Shape(string name, string[] shape)
+    public IEnumerable<Vector2Int> MapPlacementAt(Vector2Int position)
     {
-        Name = name;
-        _shape = new bool[shape.Length][];
+        List<Vector2Int> placedPositions = new();
 
-        for (var row = 0; row < shape.Length; row++)
+        foreach (Vector2Int relativePos in _shape)
         {
-            _shape[row] = new bool[shape[row].Length];
-
-            for (var col = 0; col < shape[row].Length; col++)
-            {
-                _shape[row][col] = shape[row][col] == '1';
-            }
+            placedPositions.Add(new Vector2Int(
+                position.X + relativePos.X,
+                position.Y + relativePos.Y
+            ));
         }
 
-        _shapes[name] = this;
+        return placedPositions;
     }
 }

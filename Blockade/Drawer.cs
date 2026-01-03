b48ecc3ed207e9229334a92c41d73720bb07dc80
Zantron2000 <xanderpalmer7@gmail.com>
2026-01-03 10:12:56 -0500

    Created the Drawer controller class

---
+++ Blockade/Drawer.cs
@@ -0,0 +1,78 @@
+using Raylib_cs;
+
+namespace Blockade
+{
+    internal class Drawer
+    {
+        private static readonly int BORDER_WIDTH = 20;
+        private static readonly int BORDER_HEIGHT = 20;
+        private static readonly int DRAWER_WIDTH = 500;
+        private static readonly double PARTITION_RATIO = 0.10;
+        private static readonly Color DRAWER_COLOR = Color.White;
+        private static readonly int CONTAINER_PADDING = 10;
+        private static readonly int TEXT_FONT_SIZE = 20;
+        private static readonly Color TEXT_COLOR = Color.Black;
+
+        private readonly Player _player;
+        private readonly List<Shape> _shapes;
+        private int _screenWidth;
+        private int _screenHeight;
+        private int DrawerX => _screenWidth - DRAWER_WIDTH;
+        private int DrawerY => 0;
+
+        public Drawer(Player player, List<Shape> shapes, int screenWidth, int screenHeight)
+        {
+            _player = player;
+            _shapes = shapes;
+            _screenWidth = screenWidth;
+            _screenHeight = screenHeight;
+        }
+
+        public Rectangle GetDrawerDimensions()
+        {
+            return new Rectangle(
+                DrawerX, 
+                DrawerY, 
+                DRAWER_WIDTH, 
+                _screenHeight, 
+                DRAWER_COLOR
+            );
+        }
+
+        public Rectangle GetScoreAreaDimensions()
+        {
+            return new Rectangle(
+                DrawerX + BORDER_WIDTH,
+                DrawerY + BORDER_HEIGHT,
+                DRAWER_WIDTH - 2 * BORDER_WIDTH,
+                (int)((_screenHeight - 2 * BORDER_HEIGHT) * PARTITION_RATIO),
+                Color.White
+            );
+        }
+
+        public Rectangle GetBlocksAreaDimensions()
+        {
+            return new Rectangle(
+                DrawerX + BORDER_WIDTH,
+                DrawerY + BORDER_HEIGHT + (int)((_screenHeight - 2 * BORDER_HEIGHT) * PARTITION_RATIO),
+                DRAWER_WIDTH - 2 * BORDER_WIDTH,
+                _screenHeight - 2 * BORDER_HEIGHT - (int)((_screenHeight - 2 * BORDER_HEIGHT) * PARTITION_RATIO),
+                Color.LightGray
+            );
+        }
+
+        public Text GetScoreText()
+        {
+            string scoreContent = $"{84}/{84} blocks placed | Score: {0} points";
+            int boxYEnd = (int)((_screenHeight - 2 * BORDER_HEIGHT) * PARTITION_RATIO);
+
+            return new Text(
+                scoreContent,
+                TEXT_FONT_SIZE,
+                DrawerX + BORDER_WIDTH + CONTAINER_PADDING,
+                boxYEnd - CONTAINER_PADDING,
+                TEXT_COLOR
+            );
+        }
+    }
+}

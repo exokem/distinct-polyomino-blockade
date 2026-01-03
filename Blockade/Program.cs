ad5ca0b193151789b6acd7224f36c96eb13afb3c
Zantron2000 <xanderpalmer7@gmail.com>
2026-01-03 09:56:35 -0500

    Created a scratch program to draw

--- Blockade/Program.cs
+++ Blockade/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+
+namespace Blockade
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.SetTargetFPS(60);
+            Raylib.SetConfigFlags(ConfigFlags.FullscreenMode);
+            Raylib.InitWindow(0, 0, "Distinct Polyomino Blockade");
+            DrawerView drawerView = new();
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+
+                // Draw a box that's fixed to the right hand side of the screen
+                drawerView.Draw(Raylib.GetScreenWidth(), Raylib.GetScreenHeight()); 
+
+                Raylib.DrawText("Hello, Blockade!", 10, 10, 20, Color.White);
+
+                Raylib.EndDrawing();
+            }
+            
+            Raylib.CloseWindow();
+        }
+    }
+}

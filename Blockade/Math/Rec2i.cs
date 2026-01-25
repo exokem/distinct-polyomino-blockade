786fa220aa712cc246b611a9c83bf783713f8534
Exokem <ex010@live.com>
2026-01-25 14:26:08 -0500

    Adjust struct init

--- Blockade/Math/Rec2i.cs
+++ Blockade/Math/Rec2i.cs
@@ -1,11 +1,22 @@
 namespace Blockade.Math;
 
-public struct Rec2i(int x, int y, int width, int height)
+public struct Rec2i
 {
-    public int X => x;
-    public int Y => y;
-    public int W => width;
-    public int H => height;
+    public int X { get; init; }
+    public int Y { get; init; }
+    public int W { get; init; }
+    public int H { get; init; }
+
+    public int Right => X + W;
+    public int Bottom => Y + H;
+
+    public Rec2i(int x, int y, int width, int height)
+    {
+        X = x;
+        Y = y;
+        W = width;
+        H = height;
+    }
 
     public Rec2i Shrink(int dx, int dy)
         => new(X + dx, Y + dy, W - dx * 2, H - dy * 2);
--- Blockade/Math/Vec2i.cs
+++ Blockade/Math/Vec2i.cs
@@ -1,9 +1,15 @@
 namespace Blockade.Math;
 
-public struct Vec2i(int x, int y)
+public struct Vec2i
 {
-    public int X => x;
-    public int Y => y;
+    public int X { get; init; }
+    public int Y { get; init; }
+
+    public Vec2i(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
 
     public static Vec2i Zero() => new Vec2i(0, 0);
 
